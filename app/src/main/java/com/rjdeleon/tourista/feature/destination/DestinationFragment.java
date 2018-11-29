dedce6d9db3eac6298bfc64ce7e516841f498668
pabs <pabs@alink-group.co.jp>
2018-11-29 15:43:24 +0800

    Cleanup destination fragment code

--- app/src/main/java/com/rjdeleon/tourista/feature/destination/DestinationFragment.java
+++ app/src/main/java/com/rjdeleon/tourista/feature/destination/DestinationFragment.java
@@ -1,12 +1,13 @@
 package com.rjdeleon.tourista.feature.destination;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.rjdeleon.tourista.R;
 
@@ -16,14 +17,13 @@ import com.rjdeleon.tourista.R;
  * create an instance of this fragment.
  */
 public class DestinationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_DESTINATION_ID = "DESTINATION ID";
+
+    private TextView timeField;
+    private TextView dateField;
+    private EditText notesField;
+    private FloatingActionButton saveDestButton;
 
     public DestinationFragment() {
         // Required empty public constructor
@@ -33,16 +33,13 @@ public class DestinationFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param destId Destination ID.
      * @return A new instance of fragment DestinationFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static DestinationFragment newInstance(String param1, String param2) {
+    public static DestinationFragment newInstance(String destId) {
         DestinationFragment fragment = new DestinationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_DESTINATION_ID, destId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,8 +48,7 @@ public class DestinationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // TODO: Get arguments and store globally
         }
     }
 
@@ -60,6 +56,13 @@ public class DestinationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_destination, container, false);
+        View view = inflater.inflate(R.layout.fragment_destination, container, false);
+
+        timeField = view.findViewById(R.id.timeField);
+        dateField = view.findViewById(R.id.dateField);
+        notesField = view.findViewById(R.id.notesField);
+        saveDestButton = view.findViewById(R.id.saveDestButton);
+
+        return view;
     }
 }
