e08eb61c09a435d475832928f339b536d662fd17
rjdeleon <ricardopablo.deleon@gmail.com>
2019-01-16 19:56:18 +0800

    Migrate to AndroidX

--- app/src/main/java/com/rjdeleon/tourista/TripActivity.java
+++ app/src/main/java/com/rjdeleon/tourista/TripActivity.java
@@ -1,7 +1,8 @@
 package com.rjdeleon.tourista;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TripActivity extends AppCompatActivity {
 
