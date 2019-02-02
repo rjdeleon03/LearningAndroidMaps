380c8078631f53a140642a9714fd3aae1d8ff123
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2019-02-03 06:15:52 +0800

    Correctly specify top destinations in navigation

--- app/src/main/java/com/rjdeleon/tourista/MainActivity.kt
+++ app/src/main/java/com/rjdeleon/tourista/MainActivity.kt
@@ -4,24 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI.navigateUp
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAppBarConfiguration: AppBarConfiguration
+    private lateinit var mNavController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* App bar configuration for proper drawer navigation */
+        mAppBarConfiguration = AppBarConfiguration
+                .Builder(setOf(
+                        R.id.myLocationFragment,
+                        R.id.currencyConverterFragment
+                ))
+                .setDrawerLayout(mainDrawerLayout)
+                .build()
+
+        /* Setup nav controller */
+        mNavController =  findNavController(R.id.navigation_fragment);
 
         setSupportActionBar(appToolbar)
         setupNavigation()
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navigateUp(findNavController(R.id.navigation_fragment), mainDrawerLayout)
+        return navigateUp(findNavController(R.id.navigation_fragment), mAppBarConfiguration)
     }
 
     override fun onBackPressed() {
@@ -33,19 +48,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigation() {
-        val navController = findNavController(R.id.navigation_fragment)
 
         /* Update action bar title when navigating */
-        setupActionBarWithNavController(navController, mainDrawerLayout)
+        setupActionBarWithNavController(this, mNavController, mAppBarConfiguration)
+
+        /* Setup navigation graph with drawer items */
+        setupWithNavController(mainNavigationView, mNavController)
 
         /* Handle navigation drawer item clicks */
+        mainNavigationView.menu.getItem(0).isChecked = true;
         mainNavigationView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
+
+            if (!menuItem.isChecked) {
+
+                menuItem.isChecked = true
+                when (menuItem.itemId) {
+                    R.id.menu_myLocationFragment ->
+                        mNavController.navigate(R.id.myLocationFragment)
+                    R.id.menu_currencyConverterFragment ->
+                        mNavController.navigate(R.id.currencyConverterFragment)
+                }
+            }
+
             mainDrawerLayout.closeDrawers()
             true
         }
-
-        /* Setup navigation graph with drawer items */
-        setupWithNavController(mainNavigationView, navController)
     }
 }
